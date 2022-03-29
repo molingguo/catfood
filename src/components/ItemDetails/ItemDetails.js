e23bb95f9a57a9a1ae4f6dd67c7268b73d677707
Moling Guo <moling.guo@autodesk.com>
2022-03-29 00:44:59 -0400

    display price, promotions and size

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import { Box, Card, CardHeader, CardBody, CardFooter, Button } from 'grommet';
+import { Box, Card, CardHeader, CardBody, CardFooter, Button, Image, Tag } from 'grommet';
 import { Favorite, ShareOption } from 'grommet-icons';
 
 const HOST = "http://localhost:8080";
@@ -20,13 +20,25 @@ class ItemDetails extends Component {
   }
 
   render() {
+
     return (
       <Box pad="medium" gap="medium" direction="row" wrap="true">
         {
           this.state.items.map(item =>
-            <Card height="medium" width="300px" background="light-1" gap="medium" pad="medium">
-              <CardHeader>{item.name}</CardHeader>
-              <CardBody>${item.price}</CardBody>
+            <Card height="auto" width="300px" background="light-1" gap="medium" pad="medium">
+              <CardHeader height="80px">{item.name}</CardHeader>
+              <CardBody align="center">
+                <Box height="180px"><Image fit="contain" src={item.image}></Image></Box>
+                <span>${item.price}</span>
+                <span>{ item.size }</span>
+                <Box direction="row">
+                  {
+                    item.promotions.length > 0 ? item.promotions.map(promo =>
+                      <Tag value={promo} size="small" background="#1b8b3f" border="false"></Tag>
+                    ) : <span></span>
+                  }
+                </Box>
+              </CardBody>
               <CardFooter>
                 <Button
                 icon={<Favorite color="red" />}
